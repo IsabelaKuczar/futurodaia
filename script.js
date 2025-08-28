00d1dccac6b5ec6cbc35484712325d714836aeca
IsabelaKuczar <145712871+IsabelaKuczar@users.noreply.github.com>
2025-08-28 17:13:29 -0300

    f

--- script.js
+++ script.js
@@ -2,3 +2,25 @@ const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativa");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado:
+         "Qual das áreas abaixo tem maior potencial de ser transformada pela IA nos próximos 10 anos?",
+         alternativas: [
+            "Agricultura tradicional somente",
+            "Diagnóstico médico e automação de trabalho"
+         ]
+    },
+    {
+        enunciado: "Qual é um dos maiores desafios éticos relacionados ao uso crescente da IA?",
+        alternativas: [
+            "Viés algorítmico e discriminação automatizada",
+            "Substituir todos os professores por IA",
+        ]
+    },
+     {
+        enunciado: "O que se entende por “IA Generativa”, tendência forte para o futuro da tecnologia?,
+"
+     }
+]
