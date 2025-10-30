9f278115ed68c485ffc958aa3241192b3fa60c22
IsabelaKuczar <145712871+IsabelaKuczar@users.noreply.github.com>
2025-10-30 16:25:33 -0300

    .

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
-const caixaAlternativas = document.querySelector(".caixa-alternativa");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
